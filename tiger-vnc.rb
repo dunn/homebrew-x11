8bc1e68574decbd20dcfd1ce6d66dacb03230b22
Alex Dunn <dunn.alex@gmail.com>
2015-05-22 13:13:16 -0700

    tiger-vnc 1.4.3

--- tiger-vnc.rb
+++ tiger-vnc.rb
@@ -1,25 +1,29 @@
-require 'formula'
-
 class TigerVnc < Formula
-  homepage 'http://tigervnc.org/'
-  url 'https://github.com/TigerVNC/tigervnc/archive/v1.4.0.tar.gz'
-  sha1 '36252b10912bcbef44d43c5fa7977ae97988fff3'
+  homepage "http://tigervnc.org/"
+  url "https://github.com/TigerVNC/tigervnc/archive/v1.4.3.tar.gz"
+  sha256 "0b2603db2b32dfd6e48f6f59618bd9819d187bfbb0c16218637d074a69756824"
 
-  depends_on 'cmake' => :build
-  depends_on 'gnutls' => :recommended
-  depends_on 'jpeg-turbo'
-  depends_on 'gettext'
-  depends_on 'fltk'
+  head "https://github.com/TigerVNC/tigervnc.git"
+
+  depends_on "cmake" => :build
+  depends_on "gnutls" => :recommended
+  depends_on "jpeg-turbo"
+  depends_on "gettext"
+  depends_on "fltk"
   depends_on :x11
 
   def install
-    turbo = Formula['jpeg-turbo']
+    turbo = Formula["jpeg-turbo"]
     args = std_cmake_args + %W[
       -DJPEG_INCLUDE_DIR=#{turbo.include}
       -DJPEG_LIBRARY=#{turbo.lib}/libjpeg.dylib
       .
     ]
-    system 'cmake', *args
-    system 'make install'
+    system "cmake", *args
+    system "make", "install"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/vncviewer -h 2>&1", 1)
   end
 end
